b30da2e027699d247a4ba99b6e12ec7e445ce25d
Say, The Creator <64602785+H4nnya@users.noreply.github.com>
2021-01-22 13:32:17 -0300

    Add files via upload

---
+++ CursoEmVideo/Mundo3/072 Número por Extenso.py
@@ -0,0 +1,16 @@
+"""
+exercise: 072
+objective: Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso,
+de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+"""
+
+cont = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito',
+        'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezeseis',
+        'dezesete', 'dezoito', 'desenove', 'vinte')
+while True:
+    num = int(input('digite um número entre 0 a 20: '))
+    if num < 0 or num > 20:
+        print('Tente novamente. ', end='')
+    else:
+        break
+print(f'Você digitou o número {cont[num]}.')
